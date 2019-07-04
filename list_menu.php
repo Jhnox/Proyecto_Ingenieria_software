d5bc77e6a6e6b79099c7879b2dc8327eea244038
jhnox <sebitaxxxx@gmail.com>
2019-07-04 00:05:08 -0400

    se agrega listmenu 04-07-2019

--- list_menu.php
+++ list_menu.php
@@ -1,13 +1,13 @@
         <?php include 'util/helpers.php';
         if (!isset($_SESSION['RUT_USUARIO'])) {
-            redirigir('login.php');
+          redirigir('login.php');
         }
         ?>
         <!DOCTYPE html>
         <html lang="en">
 
         <head>
-          <title>Inicio</title>
+          <title>Lista de Menu</title>
           <!-- Required meta tags -->
           <meta charset="utf-8">
           <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,6 +18,11 @@
           <link rel="stylesheet" href="w3/google-apis.css">
           <link rel="stylesheet" href="w3/w3-theme-black.css">
           <link rel="stylesheet" href="w3/w3.css">
+          <script type="text/javascript" src="js/jquery-1.2.3.min.js"></script>
+          <script type="text/javascript" src="js/jquery.func.js"></script>
+
+         
+          <link rel="stylesheet" href="css/calendar.css" media="screen">
 
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -27,7 +32,6 @@
             .w3-sidebar {
               z-index: 3;
               width: 250px;
-              top: 45px;
               bottom: 0;
               height: inherit;
             }
@@ -39,6 +43,9 @@
               font-family: 'Numans', sans-serif;
             }
           </style>
+
+          <style type="text/css">#calendar{margin:25px auto; width: auto;}</style>
+          
 
           
         </head>
@@ -69,71 +76,205 @@
 
 
           <div class="container my-4">
-            
-            <div class="w3-row w3-padding-64">
-              <div class="w3-twothird w3-container">
-                
-              </div>
-              
-            </div>
+
+
 
             <div class="w3-row">
+
+              <!-- Lista de menu a la Izquierda -->
+
               <div class="w3-twothird w3-container">
-                <h1 class="w3-text-teal">Alimentación Saludable</h1>
+                <h1 class="w3-text-teal">Lista de Menus</h1>
                 <div class="container">
-                  
-                  <div class="embed-responsive embed-responsive-16by9">
-                    <iframe width="1423" height="620" src="https://www.youtube.com/embed/WbUH_dQYWrE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  <div class="w3-third w3-container">
+                    <ul class="w3-ul w3-hoverable">
+                      <li onclick="document.getElementById('id01').style.display='block'" >Menu 1</li>
+                      <li onclick="document.getElementById('id02').style.display='block'">Menu 2</li>
+                      <li onclick="document.getElementById('id03').style.display='block'">Menu 3</li>
+                    </ul>
                   </div>
+
+
                 </div>
               </div>
+
+              <!-- Fin Lista de menu a la Izquierda -->
+
+              <!-- Calendario a la derecha -->
+
               <div class="w3-third w3-container">
-                <p class="w3-border w3-padding-large w3-padding-32 w3-center"><img class="img-thumbnail" src="img/679.jpg" alt=""></p>
-                <p class="w3-border w3-padding-large w3-padding-64 w3-center"><img class="img-thumbnail" src="img/6377.jpg" alt=""></p>
-              </div>
-            </div>
+                <br>
+                <br>
+                <div id="calendar"></div>
 
-            <div class="w3-row w3-padding-64">
-              <div class="w3-twothird w3-container">
-                <h1 class="w3-text-teal">Heading</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
-                dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-              </div>
-              <div class="w3-third w3-container">
-                <p class="w3-border w3-padding-large w3-padding-32 w3-center">ad</p>
-                <p class="w3-border w3-padding-large w3-padding-64 w3-center">AD</p>
-              </div>
-            </div>
+              </div> 
+
+              <!-- FIN Calendario a la derecha -->
+
+              <!-- Modal de informacion de los menus -->
+              
+              <div id="id01" class="w3-modal">
+               <div class="w3-modal-content w3-card-4 w3-animate-zoom">
+                <header class="w3-container w3-blue"> 
+                 <span onclick="document.getElementById('id01').style.display='none'" 
+                 class="w3-button w3-blue w3-xlarge w3-display-topright">&times;</span>
+                 <h2>Menu de 3500 calorias</h2>
+               </header>
+
+               <div class="w3-bar w3-border-bottom">
+                 <button class="tablink w3-bar-item w3-button" onclick="openCity(event, 'London')">London</button>
+                 <button class="tablink w3-bar-item w3-button" onclick="openCity(event, 'Paris')">Paris</button>
+                 <button class="tablink w3-bar-item w3-button" onclick="openCity(event, 'Tokyo')">Tokyo</button>
+               </div>
+
+               <div id="London" class="w3-container city">
+                 <h1>London</h1>
+                 <p>London is the most populous city in the United Kingdom, with a metropolitan area of over 9 million inhabitants.</p>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+               </div>
+
+               <div id="Paris" class="w3-container city">
+                 <h1>Paris</h1>
+                 <p>Paris is the capital of France.</p>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+               </div>
+
+               <div id="Tokyo" class="w3-container city">
+                 <h1>Tokyo</h1>
+                 <p>Tokyo is the capital of Japan.</p><br>
+               </div>
+
+               <div class="w3-container w3-light-grey w3-padding">
+                 <button class="w3-button w3-right w3-white w3-border" 
+                 onclick="document.getElementById('id01').style.display='none'">Close</button>
+               </div>
+             </div>
+           </div>
+
+           <!-- Fin Modal de informacion de los menus -->
 
 
-          </div>
-          <!-- Fin Contenido -->
+           <div id="id02" class="w3-modal">
+             <div class="w3-modal-content w3-card-4 w3-animate-zoom">
+              <header class="w3-container w3-blue"> 
+               <span onclick="document.getElementById('id02').style.display='none'" 
+               class="w3-button w3-blue w3-xlarge w3-display-topright">&times;</span>
+               <h2> Menu de 2000 calorias</h2>
+             </header>
 
-          <!-- Footer -->
+             <div class="w3-bar w3-border-bottom">
+               <button class="tablink w3-bar-item w3-button" onclick="openCity(event, 'Chile')">Chile</button>
+               <button class="tablink w3-bar-item w3-button" onclick="openCity(event, 'Argentica')">Argentica</button>
+               <button class="tablink w3-bar-item w3-button" onclick="openCity(event, 'Zimbambuew')">Zimbambuew</button>
+             </div>
 
-          <!-- Footer -->
-          <footer class="w3-center w3-black w3-padding-64 w3-opacity w3-hover-opacity-off">
-           
-            <div class="w3-xlarge w3-section">
-              <i class="fa fa-facebook-official w3-hover-opacity"></i>
-              <i class="fa fa-instagram w3-hover-opacity"></i>
-              <i class="fa fa-snapchat w3-hover-opacity"></i>
-              <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-              <i class="fa fa-twitter w3-hover-opacity"></i>
-              <i class="fa fa-linkedin w3-hover-opacity"></i>
-            </div>
-            <p>Powered by Juan</p>
-          </footer>
-          <!-- Fin Footer -->
+             <div id="Chile" class="w3-container city">
+               <h1>Chile</h1>
+               <p>London is the most populous city in the United Kingdom, with a metropolitan area of over 9 million inhabitants.</p>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+             </div>
 
-          <script>
-            // Script to open and close sidebar
+             <div id="Argentica" class="w3-container city">
+               <h1>Argentica</h1>
+               <p>Paris is the capital of France.</p>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+             </div>
+
+             <div id="Zimbambuew" class="w3-container city">
+               <h1>Zimbambuew</h1>
+               <p>Tokyo is the capital of Japan.</p><br>
+             </div>
+
+             <div class="w3-container w3-light-grey w3-padding">
+               <button class="w3-button w3-right w3-white w3-border" 
+               onclick="document.getElementById('id02').style.display='none'">Close</button>
+             </div>
+           </div>
+         </div>
+
+       </div>
+
+       
+     </div>
+
+     <div class="w3-row w3-padding-64">
+      <div class="w3-twothird w3-container">
+
+      </div>
+      <div class="w3-third w3-container">
+
+      </div> 
+    </div>
+
+
+
+    <!-- Fin Contenido -->
+
+    <!-- Footer -->
+
+    <!-- Footer -->
+    <footer class="w3-center w3-black w3-padding-64 w3-opacity w3-hover-opacity-off">
+
+      <div class="w3-xlarge w3-section">
+        <i class="fa fa-facebook-official w3-hover-opacity"></i>
+        <i class="fa fa-instagram w3-hover-opacity"></i>
+        <i class="fa fa-snapchat w3-hover-opacity"></i>
+        <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+        <i class="fa fa-twitter w3-hover-opacity"></i>
+        <i class="fa fa-linkedin w3-hover-opacity"></i>
+      </div>
+      <p>Powered by Juan</p>
+    </footer>
+    <!-- Fin Footer -->
+
+
+
+
+
+    <!-- Script del calendario -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script src="js/jquery-ui-datepicker.min.js"></script>
+    <script>
+      $('#calendar').datepicker({
+        inline: true,
+        firstDay: 1,
+        showOtherMonths: true,
+        dayNamesMin: ['Dom', 'Lun', 'Mar', 'Mier', 'Jue', 'Vie', 'Sab']
+      });
+    </script>
+
+
+
+
+    <script>
+            // Script para abrir y cerrar el menu lateral
             function w3_open() {
               document.getElementById("mySidebar").style.display = "block";
             }
             
             function w3_close() {
               document.getElementById("mySidebar").style.display = "none";
+            }
+          </script>
+
+
+
+          <!-- Script para abrir el modal de los menus -->
+          <script>
+            document.getElementsByClassName("tablink")[0].click();
+
+            function openCity(evt, cityName) {
+              var i, x, tablinks;
+              x = document.getElementsByClassName("city");
+              for (i = 0; i < x.length; i++) {
+                x[i].style.display = "none";
+              }
+              tablinks = document.getElementsByClassName("tablink");
+              for (i = 0; i < x.length; i++) {
+                tablinks[i].classList.remove("w3-light-grey");
+              }
+              document.getElementById(cityName).style.display = "block";
+              evt.currentTarget.classList.add("w3-light-grey");
             }
           </script>
 
--- util/helpers.php
+++ util/helpers.php
@@ -13,7 +13,7 @@
 
     function redirigir($pagina)
     {
-        header('Location: http://localhost/apps/' . $pagina);
+        header('Location: http://192.168.43.59/apps/' . $pagina);
         ob_end_flush();
     }
 
@@ -45,11 +45,11 @@
             $RUT_USUARIO = $_SESSION['RUT_USUARIO'];
             $id_rol = obtener_rol($RUT_USUARIO);
             if ($id_rol == 1) {
-                echo '<a class="w3-bar-item w3-button" href="registro.php">Lista de Menus</a>';
+                echo '<a class="w3-bar-item w3-button" href="list_menu.php">Lista de Menus</a>';
                 echo '<a class="w3-bar-item w3-button" href="registro.php">Pedidos</a>';
                 echo '<a class="w3-bar-item w3-button" href="registro.php">Contacto</a>';
             } else {
-                echo '<a class="w3-bar-item w3-button" href="registro.php">Lista de Menus</a>';
+                echo '<a class="w3-bar-item w3-button" href="list_menu.php">Lista de Menus</a>';
                 echo '<a class="w3-bar-item w3-button" href="registro.php">Pedidos</a>';
                 echo '<a class="w3-bar-item w3-button" href="registro.php">Contacto</a>';
             }
@@ -93,3 +93,106 @@
             redirigir("login.php");
         }
     }
+
+
+    class calendario{
+    var $nombre_dias = array('Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado');
+    function calendario(){
+        
+    }
+    
+    function mostrarBarra(){
+        //proximamente
+        ?>
+        <div id="barcal">
+        </div>
+        <?php
+    }
+    
+    function mostrar(){
+        date_default_timezone_set('America/Santiago');
+        $mes=date('m',time());
+        $anio=date('Y',time());
+        //dias mes anterior
+        if($mes==1){ $mes_anterior=12; $anio_anterior = $anio-1; }
+        else{ $mes_anterior = $mes-1; $anio_anterior = $anio; }
+        
+        $ultimo_dia_mes_anterior = date('t',mktime(0,0,0,$mes_anterior,1,$anio_anterior));
+        
+        $dia=1;
+        if(strlen($mes)==1) $mes='0'.$mes;
+        ?>
+        <table id="minical" cellpadding="0" cellspacing="0">
+        <thead>
+         <tr >
+          <th><?php echo $this->nombre_dias[0]?></th>
+          <th><?php echo $this->nombre_dias[1]?></th>
+          <th><?php echo $this->nombre_dias[2]?></th>
+          <th><?php echo $this->nombre_dias[3]?></th>
+          <th><?php echo $this->nombre_dias[4]?></th>
+          <th><?php echo $this->nombre_dias[5]?></th>
+          <th><?php echo $this->nombre_dias[6]?></th>
+         </tr>
+        </thead>
+        <tbody>
+        <?php
+    
+        
+        $numero_primer_dia = date('w', mktime(0,0,0,$mes,$dia,$anio)); //numero dia en semana
+        
+        $ultimo_dia = date('t', mktime(0,0,0,$mes,$dia,$anio));
+        
+        $diferencia_mes_anterior = $ultimo_dia_mes_anterior - ($numero_primer_dia-1);
+        
+        $total_dias=$numero_primer_dia+$ultimo_dia;
+        $diames=1;
+        //$j dias totales (dias que empieza a contarse el 1º + los dias del mes)
+        $j=1;
+        while($j<=$total_dias){
+            //if($j%2==0) echo "<tr class=\"odd\"> \n";
+            //else 
+            echo "<tr> \n";
+            //$i contador dias por semana
+            $i=0;
+            $k=1; //dias proximo mes
+            while($i<7){
+                if($j<=$numero_primer_dia){
+                    echo "<td class=\"disabled\"> \n";
+                    echo "<div class=\"headbox\"> \n";
+                    echo $diferencia_mes_anterior;
+                    echo "</div>";
+                    echo "<div class=\"bodybox\"></div> \n";
+                    echo "</td> \n";
+                    $diferencia_mes_anterior++;
+                }elseif($diames>$ultimo_dia){
+                    echo "<td class=\"disabled\"> \n";
+                    echo "<div class=\"headbox\"> \n";
+                    echo $k;
+                    echo "</div>";
+                    echo "<div class=\"bodybox\"></div> \n";
+                    echo"</td> \n";
+                    $k++; //dias proximo mes
+                }else{
+                    if($diames<10) $diames_con_cero='0'.$diames;
+                    else $diames_con_cero=$diames;
+    
+                    echo "<td>";
+                    echo "<div class=\"headbox\"> \n";
+                    echo $diames;
+                    echo "</div> \n";
+                    echo "<div class=\"bodybox\"></div> \n";
+                    echo "</td> \n";
+                    $diames++;
+                }
+                $i++;
+                $j++;
+            }
+            echo "</tr> \n";
+        }
+        ?>
+         </tbody>
+        </table>
+        <?php
+    }
+
+}
